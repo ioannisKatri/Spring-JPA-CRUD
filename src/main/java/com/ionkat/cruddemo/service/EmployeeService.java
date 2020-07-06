3e9661d9328c47cf71b55242a1498991d237918e
IoannisKatri <ioanniskatri@hotmail.com>
2020-07-06 15:26:31 +0100

    SPRING JPA CRUD

---
+++ src/main/java/com/ionkat/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.ionkat.cruddemo.service;
+
+import java.util.List;
+
+import com.ionkat.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+	
+}
